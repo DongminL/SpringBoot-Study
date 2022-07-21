ecab8737f0a2c8dc89e22182810221bd53e32d8d
DongminL <milkskfk12@gmail.com>
2022-07-21 10:57:58 +0900

    IoC DI

---
+++ BoardProject/src/main/java/com/example/firstproject/ioc/Ingredient.java
@@ -0,0 +1,14 @@
+package com.example.firstproject.ioc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+public class Ingredient {
+    private String name;
+}
